595f7bc88fc8d69bc3a273536310d851c9b7109d
greysoh <dev@hofers.cloud>
2023-09-06 09:13:55 -0400

    feature: Adds user database, and creation system.
    I don't have internet (school), and I forgot to install bcrypt on npm
    , so sorry in advance for terrible auth.

--- client/index.mjs
+++ client/index.mjs
@@ -1,3 +1,5 @@
+import process from "node:process";
+
 import Datastore from "nedb-promises";
 import express from "express";
 import openpgp from "openpgp";
@@ -19,10 +21,45 @@ console.log(`
 ProjectSFP - Easily forward ports over the network securely.
 You are running ProjectSFP Client, intended for forwarding ports to the server.`);
 
+const portForwardDB = Datastore.create("./ports.db");
 const clientDB = Datastore.create("./client.db");
+const usersDB = Datastore.create("./users.db");
+
+const sessionTokens = [
+  {
+    username: "example",
+    token: ""
+  }
+]
 
 const app = express();
 app.use(express.json());
+
+if (await usersDB.count() == 0 || process.env.INIT_CREATE_USER) {
+  // Create a new user
+  if (!process.env.INIT_USERNAME || !process.env.INIT_PASSWORD) {
+    console.error("\nCannot initialize! You did not specify a username and password to create, as this is the first time running!");
+    console.error("Set the environment variables: INIT_USERNAME, INIT_PASSWORD");
+    process.exit(1);
+  }
+
+  // Just in case, make sure a user with that username does NOT exist.
+  const userSearch = await usersDB.findOne({
+    username: process.env.INIT_USERNAME
+  });
+
+  if (userSearch) {
+    console.error("\nCannot initialize! A user with that username already exists.");
+    process.exit(1);
+  }
+
+  await usersDB.insertOne({
+    username: process.env.INIT_USERNAME,
+    password: process.env.INIT_PASSWORD,
+
+    isAdministrator: process.env.INIT_SHOULD_BE_REGULAR_USER ? false : true
+  });
+}
 
 app.post("/api/v1/pair", async(req, res) => {
   if (!req.body.url) {
