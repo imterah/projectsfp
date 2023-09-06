a4f10621bdf27404063f5d2409607ff51250f4c5
greysoh <dev@hofers.cloud>
2023-09-06 12:46:11 -0400

    fix: Fixes sha256 number generation

--- client/libs/sha256.mjs
+++ client/libs/sha256.mjs
@@ -1,6 +1,6 @@
 import crypto from "node:crypto";
 
 export function sha256(val) {
-  const hash = crypto.createHash("sha256", val).digest("hex");
+  const hash = crypto.createHash("sha256").update(val.toString()).digest("hex");
   return hash;
 }
